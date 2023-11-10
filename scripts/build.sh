ffb29650c5547138c2909ba3486aa7e1d265414d
Maximilian Weiler <16721506+maweil@users.noreply.github.com>
2023-11-10 22:29:40 +0000

    chore: Remove gschemas.compiled from ZIP during build
    - Shall not be included for GNOME45 anymore

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+set -e
+line="----------------------------------------"
+echo $line
+echo "Cleanup"
+echo $line
+rm ./dist/*.zip || exit 0
 mkdir -p ./dist
+echo "Building Extension ZIP"
+echo $line
 gnome-extensions pack \
     --extra-source ./mumblePing.js \
     --extra-source ./icons \
     --podir ./po \
     -o dist \
     --force
+echo "Removing compiled settings schema (not allowed for GNOME 45)"
+echo $line
+zip -d dist/mumble-ping*.zip "/schemas/gschemas.compiled"
+echo $line
+echo "Done!"
