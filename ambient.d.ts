3e12bc737b9ce9f814dc90f2a289175f7459cda9
Maximilian Weiler <16721506+maweil@users.noreply.github.com>
2024-05-09 17:08:07 +0000

    chore: Add source of ambient.d.ts
    - Update deploy script to skip build if possible

--- ambient.d.ts
+++ ambient.d.ts
@@ -1,3 +1,4 @@
+// Source: https://gjs.guide/extensions/development/typescript.html#ambient-d-ts
 import '@girs/gjs';
 import '@girs/gjs/dom';
 import '@girs/gnome-shell/ambient';
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+set -e
 extension_name="mumble-ping@maweil.github.com"
-./scripts/build.sh
-gnome-extensions install -f "./dist/${extension_name}.shell-extension.zip"
+extension_path="./dist/${extension_name}.shell-extension.zip"
+if [ ! -f "$extension_path" ]
+then
+    ./scripts/build.sh
+fi
+gnome-extensions install -f "$extension_path"
